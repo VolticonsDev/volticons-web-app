36f7d69386188db180484feb868b6cf046004ef2
VolticonsDev <volticonsteam@gmail.com>
2025-11-20 16:58:41 -0300

    fixeamos teamfilter y agregamos category

--- src/components/TeamFilter.jsx
+++ src/components/TeamFilter.jsx
@@ -1,32 +1,33 @@
-import React from "react";
-const TeamFilter = ({ categories, selectedCategory, onSelectedCategory}) => {
+import React from 'react';
+
+const TeamFilter = ({ categories, selectedCategory, onSelectCategory }) => {
   return (
-     <div className="flex flex-wrap gap-3 p-4 bg-header-bg rounded-lg shadow-xl mb-8 justify-center border border-volt-violet">
+    <div className="flex flex-wrap gap-3 p-4 bg-header-bg rounded-lg shadow-xl mb-8 justify-center border border-volt-violet">
       <button
-           onClick={() => onSelectedCategory('All')}
-           className={`px-4 py-2 rounded-full text-sm font-bold transition-all duration-200 ${
-            selectedCategory === 'All'
+        onClick={() => onSelectCategory('All')}
+        className={`px-4 py-2 rounded-full text-sm font-bold transition-all duration-200 ${
+          selectedCategory === 'All'
             ? 'bg-volt-orange text-dark shadow-md shadow-volt-orange/50'
             : 'bg-dark-bg text-gray-300 hover:bg-volt-violet'
-}`}
->
-  Todos los partidos
-  </button>
+        }`}
+      >
+        Todos los Partidos
+      </button>
 
-  {categories.map((category) => (
-    <button
-       key={category}
-       onClick={() => onSelectedCategory(category)}
-       className={`px-4 py-2 rounded-full text-sm font-bold transition-all duration-200 ${
-        selectedCategory === category
-        ? 'bg-volt-orange text-dark shadow-md shadow-volt-orange/50'
-        : 'bg-dark0-bg text-gray-300 hover:bg-volt-violet'
-       }`}
-       >
-        {category}
+      {categories.map((category) => (
+        <button
+          key={category}
+          onClick={() => onSelectCategory(category)}
+          className={`px-4 py-2 rounded-full text-sm font-bold transition-all duration-200 ${
+            selectedCategory === category
+              ? 'bg-volt-orange text-dark shadow-md shadow-volt-orange/50'
+              : 'bg-dark-bg text-gray-300 hover:bg-volt-violet'
+          }`}
+        >
+          {category}
         </button>
-  ))}
-  </div>
+      ))}
+    </div>
   );
 };
 
